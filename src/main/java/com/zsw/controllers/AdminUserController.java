9e91464d421643e47c89bd504c703b6f375e4336
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-04-26 00:33:14 +0800

    new

---
+++ src/main/java/com/zsw/controllers/AdminUserController.java
@@ -0,0 +1,13 @@
+package com.zsw.controllers;
+
+import com.zsw.utils.UserStaticURLUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhangshaowei on 2020/4/26.
+ */
+@Controller
+@RequestMapping(UserStaticURLUtil.adminUserController)
+public class AdminUserController {
+}
